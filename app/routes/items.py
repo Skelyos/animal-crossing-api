2ec3b623be44c8642e74e96e57b4b3c156b66156
Skelyos <pat123john123@gmail.com>
2022-07-20 13:55:50 +0100

    Added in search by item/villager name routes

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter, status
-
 from app.schemas import item
-
+from fastapi import APIRouter, status
 from ..core.database import mongo_database
 
 router = APIRouter()
@@ -13,7 +11,6 @@ def get_all_item():
     collection = mongo_database[collection_name]
     found_items_cursor = list(collection.find())
     found_items: list[item.Item] = [item.Item(**found_item_cursor) for found_item_cursor in found_items_cursor]
-
     return found_items
 
 
@@ -22,14 +19,13 @@ def get_item_by_category(search_category: str):
     collection = mongo_database[collection_name]
     found_items_cursor = list(collection.find({'category': search_category.lower()}))
     found_items: list[item.Item] = [item.Item(**found_item_cursor) for found_item_cursor in found_items_cursor]
-
     return found_items
 
 
-@router.get("/name", tags=["items"], status_code=status.HTTP_200_OK)
-def get_item_by_name(search_name: str):
-    collection = mongo_database[collection_name]    
-    found_items_cursor = list(collection.find({"$id": {"$search": search_name.lower()}}))
+@router.get("/search", tags=["items"], status_code=status.HTTP_200_OK)
+def get_item_by_search(search_text: str):
+    collection = mongo_database[collection_name]
+    found_items_cursor = list(collection.find({"$text": {"$search": search_text}}))
     found_items: list[item.Item] = [item.Item(**found_item_cursor) for found_item_cursor in found_items_cursor]
     return found_items
 
--- app/routes/villagers.py
+++ app/routes/villagers.py
@@ -1,8 +1,38 @@
+from app.schemas import villager
 from fastapi import APIRouter, status
+from ..core.database import mongo_database
 
 router = APIRouter()
 
+collection_name = 'villagers'
 
-@router.get("/", tags=["villagers"], status_code=status.HTTP_200_OK)
-def get_villager():
-    return 'villager'
+@router.get("/all", tags=["villagers"], status_code=status.HTTP_200_OK)
+def get_all_villagers():
+    collection = mongo_database[collection_name]
+    found_items_cursor = list(collection.find())
+    found_items: list[villager.Villager] = [villager.Villager(**found_item_cursor) for found_item_cursor in found_items_cursor]
+    return found_items
+
+
+@router.get("/species", tags=["villagers"], status_code=status.HTTP_200_OK)
+def get_villagers_by_category(search_species: str):
+    collection = mongo_database[collection_name]
+    found_items_cursor = list(collection.find({'species': search_species.lower()}))
+    found_items: list[villager.Villager] = [villager.Villager(**found_item_cursor) for found_item_cursor in found_items_cursor]
+    return found_items
+
+
+@router.get("/gender", tags=["villagers"], status_code=status.HTTP_200_OK)
+def get_item_by_gender(search_gender: str):
+    collection = mongo_database[collection_name]
+    found_items_cursor = list(collection.find({'gender': search_gender.lower()}))
+    found_items: list[villager.Villager] = [villager.Villager(**found_item_cursor) for found_item_cursor in found_items_cursor]
+    return found_items
+
+
+@router.get("/search", tags=["villagers"], status_code=status.HTTP_200_OK)
+def get_item_by_name(search_text: str):
+    collection = mongo_database[collection_name]
+    found_items_cursor = list(collection.find({"$text": {"$search": search_text}}))
+    found_items: list[villager.Villager] = [villager.Villager(**found_item_cursor) for found_item_cursor in found_items_cursor]
+    return found_items
